38b12ae19a21bb96d52ef109220704aca1b0e0e2
Ello17 <azzellofabian@gmail.com>
2026-06-22 12:42:50 +0700

    frist commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function showSidebar() {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.classList.add("active");
+}
+function hideSidebar() {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.classList.remove("active");
+}
